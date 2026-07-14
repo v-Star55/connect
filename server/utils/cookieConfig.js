1fc309215f24d5b17364b25877d57c60f7ec0ac2
Vaibhav Chauhan <106245961+v-Star55@users.noreply.github.com>
2026-07-14 20:54:01 +0530

    bug fix

--- server/utils/cookieConfig.js
+++ server/utils/cookieConfig.js
@@ -1,10 +1,10 @@
-const isProd = process.env.NODE_ENV === "production" || process.env.RENDER === "true";
+
 
 export const getCookieOptions = (maxAge) => {
   const options = {
     httpOnly: true,
-    secure: isProd,
-    sameSite: "lax",
+    secure: true,
+    sameSite: "none"
   };
   if (maxAge !== undefined) {
     options.maxAge = maxAge;
